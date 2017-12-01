02857735b0f7844da87fa405a94372d102bcff0e
Alex Murray <Alexisntonfire1@gmail.com>
2017-11-30 20:27:12 -0600

    Add files via upload

---
+++ api_keys.py
@@ -0,0 +1,12 @@
+# Twitter
+twitter_consumer_key = "PYC1YCrXl4rdOzoPytM1O0ul3"
+twitter_consumer_key_secret = "QbloiGjVR2XFYF8qk6lJCj3bK0Vgj5wO2e92XKRuGngaz7npZj"
+twitter_access_token = "930592555451998213-kKafVVF8UDqk1oRQKj8g0piWj8Joy5p"
+twitter_access_token_secret = "OEUMI5DDGigNxZl7Quev0LoqHFOfozaTiHGa5SuYUey5s"
+
+# Marvel API
+public_key = "4b50341fbc11d3cc2bbc8408e8ec114a"
+Private_key = "1a950e235feb7af8dbbdd64219a6302dda71f535"
+
+# Open Movie Database
+api_key = "4b09af53"
